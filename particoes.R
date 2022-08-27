437598f31913c7c44161095030aa8c4f1699a8d0
Dalyana Ribeiro <duilianobrega@gmail.com>
2022-08-27 00:27:16 -0300

    Etrutura de repetição com a base de dados: mtcars

--- particoes.R
+++ particoes.R
@@ -15,12 +15,12 @@ mediaBootsNormal10 <- replicate(10, mean(sample(distNormalSimulacao, 10, replace
 
 mediaBootsNormal50 <- replicate(50, mean(sample(distNormalSimulacao, 10, replace = TRUE)))
 
-mediaBootsNormal100 <- replicate(100, mean(sample(distNormalSimulacao, 10, replace = TRUE)) 
+mediaBootsNormal100 <- replicate(100, mean (sample(distNormalSimulacao, 10, replace = TRUE)),
 
 # vamos comparar???
 
 mean(mediaBootsNormal10) 
-mean(mediaBootsNormal50) 
+mean(mediaBootsNormal50)
 mean(mediaBootsNormal100) 
 mean(distNormalSimulacao) 
 
@@ -28,8 +28,8 @@ mean(distNormalSimulacao)
 install.packages('caret', dependencies = T) 
 library(caret)
 
-# partições de dados
+# partições de dado
 particaoDistNormal <- createDataPartition (1:length(distNormalSimulacao), p=.7) 
-treinoDistNormal <- distNormalSimulacao[unlist(particaoDistNormal)] 
-testeDistNormal <- distNormalSimulacao[- unlist(particaoDistNormal)]
+treinoDistNormal <- distNormalSimulacao[unlist(particaoDistNormal)
+testeDistNormal <- distNormalSimulacao[- unlist(particaoDistNormal)
 
