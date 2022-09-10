efe7c2fb7a6e45f3c8030887b66bb74e0507d7b9
Dalyana Ribeiro <duilianobrega@gmail.com>
2022-09-10 14:27:07 -0300

    extração

---
+++ extracao.R
@@ -0,0 +1,26 @@
+# etl na prática
+
+#### Staging area e uso de memória
+
+
+for (itm in ls()) { 
+  print(formatC(c(itm, object.size(get(itm))), 
+                format="d", 
+                width=30), 
+        quote=F)
+}
+
+for (itm in ls()) { 
+  print(formatC(c(itm, object.size(get(itm))), 
+                format="d", 
+                width=30), 
+        quote=F)
+}
+
+# Os objetos mais pesados são a base unificada, seguida da base relativa ao ano de 2019.
+
+# Remoção de objetos desnecessários
+
+rm(list = c('sinistrosRecife2020Raw', 'sinistrosRecife2021Raw', 
+            'sinistrosRecife2019Raw', 'colunas_iguais', 'colunas_iguais_2'))
+
