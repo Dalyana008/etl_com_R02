248ef370aa7db0d8fcaaaea0932edb7173c33a10
Dalyana Ribeiro <duilianobrega@gmail.com>
2022-09-14 15:10:14 -0300

    Correção

--- estruturas_de_fatores.R
+++ estruturas_de_fatores.R
@@ -3,30 +3,8 @@
 # criando fatores (estruturas)
 # Códigos do livro An Introduction to data cleaning with R
 
-class(c("FM", "def"))
-class(1:10)
-class(c(pi, exp(1)))
-class(factor(c("FM", "def")))
-
-vrNumeric <- c("7", "7*", "7.0", "7,0")
-is.numeric(vrNumeric)
-as.numeric(vrNumeric)
-as.integer(vrNumeric)
-as.character(vrNumeric)
-
-is.na(as.numeric(vrNumeric))
-
-vrFactor <- factor(c("M", "F", "M", "F", "M"))
-levels(vrFactor)
-
-gender <- c(1, 0, 1, 0, 1,0)
-recode <- c(male = 1, female = 0)
-(gender <- factor(gender, levels = recode, labels = names(recode)))
-
-(gender <- relevel(gender, ref = "female"))
-
-age <- c(28, 57, 30, 38, 70, 50)
-(gender <- reorder(gender, age))
-
-attr(gender, "scores") <- NULL
-gender
+# criando fator
+TimesserieC <- c(1, 2, 0, 1, 2, 2, 2, 2, 1, 0, 1, 1, 1, 1)
+recode <- c(Nautico = 1, operario = 2)
+TimesserieC<- factor(TimesserieC, levels = recode, labels = names(recode))
+str(TimesserieC)
